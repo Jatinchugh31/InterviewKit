cfa800011d9fd2e8ef5da2cbfb8b66bf6cf9d2a4
jatin chugh <jatin.chugh@dltapps.co.uk>
2024-12-05 16:30:07 +0530

    chain of responsibility

---
+++ DesignPattern/chainOfResponsiblity/CheckUserAccess.java
@@ -0,0 +1,21 @@
+package DesignPattern.chainOfResponsiblity;
+
+public class CheckUserAccess extends Middleware{
+    //DBInstance
+
+//    CheckUserAccess(Server server){
+//        this.server = server;
+//    }
+
+    @Override
+    public boolean check(String email, String password) {
+        if(!checkInDB(email,password)){
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkInDB(String email, String password) {
+      return true;
+    }
+}
